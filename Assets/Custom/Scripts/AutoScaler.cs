43c2c870761a95ac91a031b5592d55d1237b2a6c
HennysTous <tf2hennys@gmail.com>
2022-06-29 17:00:53 -0500

    Rezise_Test1
    Test de Rezise numero 1
    
    06/29/2022 - 5:00 PM

--- Assets/Custom/Scripts/AutoScaler.cs
+++ Assets/Custom/Scripts/AutoScaler.cs
@@ -2,25 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class AutoScaler : MonoBehaviour
 {
-    [SerializeField] private float defaultHeight = 2f;
-    [SerializeField] private Camera camera;
+    [SerializeField] private float defaultHeight = 1;
+    [SerializeField] Camera m_camera;
+    private float minHeight=0f;
+
+    public Camera camera
+    {
+        get => m_camera;
+        set => m_camera = value;
+    }
+
+    private void Awake()
+    {
+
+        if (m_camera == null)
+        {
+            var mainCamera = Camera.main;
+            if (mainCamera != null)
+                m_camera = mainCamera;
+        }
+    }
 
     private void Start()
     {
-        camera = GetComponent<Camera>();
+
     }
 
-    private void Resize() {
-        float headHeight = camera.transform.localPosition.y;
-        float scale = defaultHeight / headHeight;
-        transform.localScale = Vector3.one * scale;
-    }
-
-    private void OnEnable()
+    void Resize()
     {
+
+        float headHeight = m_camera.transform.localPosition.y;
+        float scale = headHeight / defaultHeight ;
+        transform.localScale = Vector3.one * scale;
+
+        Debug.Log("" + transform.localScale);
+
+    }
+
+    /*void ReScale() {
+
+        float scale = defaultHeight - minHeight;
+        float headHeight = camera.transform.localPosition.y;
+        transform.localScale = Vector3.one * scale;
+
+    }*/
+
+    IEnumerator ScaleWaiter()
+    {
+
+        yield return new WaitForSeconds(3);
         Resize();
     }
+    public void OnEnable()
+    {
+        StartCoroutine(ScaleWaiter());
 
+    }
+    private void Update()
+    {
+
+
+    }
 }
